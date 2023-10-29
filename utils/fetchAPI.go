6524ad138839a9928a40d15adda9d8d6c1aca0b9
KotonBads <kotonbads@duck.com>
2023-10-29 20:23:47 +0800

    add function for fetching the api

---
+++ utils/fetchAPI.go
@@ -0,0 +1,45 @@
+package utils
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func FetchLaunchMeta(launchdata LaunchBody) (response LaunchMeta, err error) {
+	url := "https://api.lunarclientprod.com/launcher/launch"
+
+	params := map[string]string{
+		"hwid":             "0",
+		"launch_type":      "OFFLINE",
+		"branch":           "master",
+		"launcher_version": "3.0.0",
+		"os":               launchdata.OS,
+		"arch":             launchdata.Arch,
+		"version":          launchdata.Version,
+		"module":           launchdata.Module,
+	}
+
+	jsonVal, _ := json.Marshal(params)
+	var res LaunchMeta
+
+	client := &http.Client{}
+
+	if req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonVal)); err == nil {
+		req.Header.Set("Content-Type", "application/json")
+		// req.Header.Set("User-Agent", "Lunar Client Launcher v2.16.1")
+
+		if resp, err := client.Do(req); err == nil {
+			body, _ := io.ReadAll(resp.Body)
+			fmt.Println(string(body))
+
+			if err := json.Unmarshal(body, &res); err != nil {
+				fmt.Printf("Couldn't Unmarshal the response: %s\n", err)
+			}
+			return res, err
+		}
+	}
+	return res, err
+}
---
+++ utils/structs.go
@@ -0,0 +1,45 @@
+package utils
+
+type LaunchBody struct {
+	OS      string
+	Version string
+	Module  string
+	Arch    string
+}
+
+type Artifacts struct {
+	Name string `json:"name"`
+	Sha1 string `json:"sha1"`
+	Url  string `json:"url"`
+	Type string `json:"type"`
+}
+
+type LaunchMeta struct {
+	Success        bool `json:"success"`
+	LaunchTypeData struct {
+		Artifacts []Artifacts `json:"artifacts"`
+		MainClass string      `json:"mainClass"`
+	} `json:"launchTypeData"`
+	Licenses []struct {
+		File string `json:"file"`
+		URL  string `json:"url"`
+		Sha1 string `json:"sha1"`
+	} `json:"licenses"`
+	Textures struct {
+		IndexURL  string `json:"indexUrl"`
+		IndexSha1 string `json:"indexSha1"`
+		BaseURL   string `json:"baseUrl"`
+	} `json:"textures"`
+	Jre struct {
+		Download struct {
+			URL       string `json:"url"`
+			Extension string `json:"extension"`
+		} `json:"download"`
+		ExecutablePathInArchive []string    `json:"executablePathInArchive"`
+		CheckFiles              [][]string  `json:"checkFiles"`
+		ExtraArguments          []string    `json:"extraArguments"`
+		JavawDownload           interface{} `json:"javawDownload"`
+		JavawExeChecksum        interface{} `json:"javawExeChecksum"`
+		JavaExeChecksum         string      `json:"javaExeChecksum"`
+	} `json:"jre"`
+}
