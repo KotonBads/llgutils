d6d90b212b440df48ee6a991824dff76e09fbec6
KotonBads <kotonbads@duck.com>
2023-10-30 22:18:06 +0800

    add some debuggin print statements

--- tests/artifacts.go
+++ tests/artifacts.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"fmt"
+
 	utils "github.com/KotonBads/llgutils"
 )
 
 func main() {
 	launchbody := utils.LaunchBody{
-		OS: "linux",
-		Arch: "x64",
+		OS:      "linux",
+		Arch:    "x64",
 		Version: "1.8.9",
-		Module: "forge",
+		Module:  "forge",
 	}
 	if launchmeta, err := utils.FetchLaunchMeta(launchbody); err == nil {
+		fmt.Println(launchmeta)
 		launchmeta.DownloadArtifacts("temp")
+	} else {
+		fmt.Println(err)
 	}
 }
