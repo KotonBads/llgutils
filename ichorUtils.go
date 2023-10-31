f64f058f744371205d6f94718c595f07295ce82a
KotonBads <kotonbads@duck.com>
2023-10-31 19:44:12 +0800

    add natives to cases

--- ichorUtils.go
+++ ichorUtils.go
@@ -2,15 +2,17 @@ package llgutils
 
 import "fmt"
 
-func (data LaunchMeta) SortFiles(path string) (classpath []string, external []string) {
+func (data LaunchMeta) SortFiles(path string) (classpath []string, external []string, natives []string) {
 	for _, val := range data.LaunchTypeData.Artifacts {
 		switch val.Type {
 		case "CLASS_PATH":
 			classpath = append(classpath, fmt.Sprintf("%s/%s", path, val.Name))
 		case "EXTERNAL_FILE":
 			external = append(external, fmt.Sprintf("%s/%s", path, val.Name))
+		case "NATIVES":
+			natives = append(natives, fmt.Sprintf("%s/%s", path, val.Name))
 		}
 	}
 
-	return classpath, external
+	return classpath, external, natives
 }
